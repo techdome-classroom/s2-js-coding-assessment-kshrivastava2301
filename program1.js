c7cc9a87ee622dc4a1f4d934cb1d1bfe2d90b6bf
khushi shrivastava <khushishrivastava2301@gmail.com>
2024-09-30 14:04:13 +0530

    completed

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const openBrackets = '({[';
+    const closeBrackets = ')}]';
+    const bracketPairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
     
+    for (let char of s) {
+        if (openBrackets.includes(char)) {
+            stack.push(char);
+        } else if (closeBrackets.includes(char)) {
+            if (stack.length === 0 || stack.pop() !== bracketPairs[char]) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
